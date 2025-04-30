9ba9fb738eb3f6045c4ae9be898d83b9760c5fd4
iru-codes <aixairupe992@gmail.com>
2025-04-30 12:42:58 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import DiceCard from './components/DiceCard/DiceCard';
 
-function App() {
+const DICE_TYPES = [4, 6, 8, 10, 12, 20, 100];
+
+export default function App() {
+  const [diceCounts, setDiceCounts] = useState({
+    4: 0,
+    6: 0,
+    8: 0,
+    10: 0,
+    12: 0,
+    20: 0,
+    100: 0
+  });
+
+  const updateDiceCount = (faces, value) => {
+    setDiceCounts(prev => ({ ...prev, [faces]: value }));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='titulo'>Dice-Roll App</h1>
+      <h2 className='subtitulo'>Customizá tu tirada!</h2>
+
+      <div className='dados'>
+        {DICE_TYPES.map(faces => (
+          <DiceCard
+            key={faces}
+            faces={faces}
+            count={diceCounts[faces]}
+            onCountChange={updateDiceCount}
+          />
+        ))}
     </div>
+  </div>
   );
 }
-
-export default App;
