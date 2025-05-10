e32486e33b8a2b0938ed197d59eb0e0f5e521c54
iru-codes <aixairupe992@gmail.com>
2025-05-09 22:40:58 -0300

    Integración de TS en componente ResetButton

--- src/components/ResetButton/ResetButton.tsx
+++ src/components/ResetButton/ResetButton.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./ResetButton.css";
 
-export default function ResetButton({ onReset }) {
-    return (
-        <button onClick={onReset} className="reset">
-            Reiniciar!
-        </button>
-    );
+type ResetButtonProps = {
+  onReset: () => void
 }
+
+const ResetButton: React.FC<ResetButtonProps> = ({ onReset }) => {
+  return <button onClick={onReset}>Resetear</button>
+}
+
+export default ResetButton
