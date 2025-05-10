f59f73271f5988e67bb4847a1f4a17809548622c
iru-codes <aixairupe992@gmail.com>
2025-05-09 22:41:21 -0300

    Integración de TS en componente DiceRollButton

--- src/components/DiceRollButton/DiceRollButton.tsx
+++ src/components/DiceRollButton/DiceRollButton.tsx
@@ -1,52 +1,68 @@
 import React from "react";
+import type { FullDiceResult } from "../ResultDisplay/ResultDisplay"
+import type { Preferences } from "../DicePreferences/DicePreferences"
 
-export default function DiceRollButton({ diceCounts, preferences, onRoll }) {
-    const rollDice = () => {
-        const results = []
-        const { sumatoria, comparador, objetivo, modificador, multiples } = preferences;
+export type DiceCounts = Record<number, number>
 
-        const validarCondiciones = objetivo > 0 && (comparador === "mayor" || comparador === "menor")
-
-        Object.entries(diceCounts).forEach(([faces, veces ]) => {
-          const caras = parseInt(faces)
-          const n = parseInt(veces)
-          if (n <= 0) return
-          const tiradas = []
-          let suma  = 0
-
-          for (let i = 0; i < n; i++) {
-            const base = Math.floor(Math.random() * caras) + 1
-            const tirada = base + modificador
-            tiradas.push({ base, tirada, modificador })
-            suma += tirada
-          }
-
-          const resultado = {
-            caras,
-            tiradas,
-            suma,
-            exitos:
-            validarCondiciones && !sumatoria
-                ? tiradas.map(({ tirada }) => 
-                    comparador  === "mayor" ? tirada >= objetivo : tirada <= objetivo)
-                : null,
-            exitoTotal:
-                validarCondiciones && sumatoria
-                    ? comparador === "mayor"
-                        ? suma >= objetivo
-                        : suma <= objetivo
-                    : null,
-          }
-
-            results.push(resultado)
-        })
-
-        onRoll(results)
-    }
-
-    return (
-        <button onClick={rollDice} id="dice_roll">
-          ¡Tirar dados!
-        </button>
-    )
+type DiceRollButtonProps = {
+  diceCounts: DiceCounts
+  preferences: Preferences
+  onRoll: (results: FullDiceResult[]) => void
 }
+
+type Tirada = { base: number; tirada: number; modificador: number };
+
+const DiceRollButton: React.FC<DiceRollButtonProps> = ({ diceCounts, preferences, onRoll }) => {
+  const rollDice = () => {
+    const results: FullDiceResult[] = []
+    const { sumatoria, comparador, objetivo, modificador } = preferences
+
+    const validarCondiciones = objetivo > 0 && (comparador === "mayor" || comparador === "menor")
+
+    Object.entries(diceCounts).forEach(([faces, veces]) => {
+      const caras = parseInt(faces)
+      const n = parseInt(String(veces))
+      if (n <= 0) return
+
+      const tiradas: Tirada[] = []
+      let suma = 0
+
+      for (let i = 0; i < n; i++) {
+        const base = Math.floor(Math.random() * caras) + 1
+        const tirada = base + modificador
+        tiradas.push({ base, tirada, modificador })
+        suma += tirada
+      }
+
+      const resultado: FullDiceResult = {
+        caras,
+        tiradas,
+        suma,
+        exitos:
+          validarCondiciones && !sumatoria
+            ? tiradas.map(({ tirada }) =>
+                comparador === "mayor" ? tirada >= objetivo : tirada <= objetivo
+              )
+            : undefined,
+        exitoTotal:
+          validarCondiciones && sumatoria
+            ? comparador === "mayor"
+              ? suma >= objetivo
+              : suma <= objetivo
+            : null
+      }
+
+      results.push(resultado)
+    })
+
+    onRoll(results)
+  }
+
+  return (
+    <button onClick={rollDice} id="dice_roll">
+      ¡Tirar dados!
+    </button>
+  )
+}
+
+export default DiceRollButton
